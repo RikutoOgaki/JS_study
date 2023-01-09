5b4cd481065311b333968141fe03bb015555298f
Rikuto <orikutobasuke@gmail.com>
2023-01-09 12:42:21 +0900

    js_04 add

--- js/drill04.js
+++ js/drill04.js
@@ -16,26 +16,6 @@ drill04 - 消費税の出力
 
 */
 
-const result = document.getElementById("result");
-const price = document.getElementById("price");
-const btn = document.getElementsByTagName("input");
-console.log(result);
-
-btn[1].addEventListener('click', ()=>{
-    let flg = Math.sign(price.value);
-    console.log(Math.sign(price.value));
-    console.log(price.value);
-
-    if(flg == 1){
-        result.innerText = price.value + "の税込価格は"+ (Number(price.value) + Number(price.value*0.1)) + "です";
-    }
-    else if(isNaN(price.value)){
-        result.classList.add("err");
-        result.innerText = "数値を入力してください";
-    }
-    else{
-        result.classList.add("err");
-        result.innerText = "有効な数値を代入してください"
-    }
-});
+const pr = document.getElementById("price");
+const re = document.getElementById("result");
 
